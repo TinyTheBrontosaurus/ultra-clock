3ac9a194a0f409ce59ddedee944fa0afcc5f0772
Eric Nelson <big89e@gmail.com>
2021-02-08 19:22:38 -0500

    Fix markdown

--- app/about.js
+++ app/about.js
@@ -7,103 +7,106 @@ import {
   Linking,
 } from 'react-native';
 
+import Markdown from 'react-native-markdown-display';
+
+const mathIsHardTxt1 = `
+Whenever I run, I am frequently doing math in my head. Calculating my pace. Calculating how far I have left to go. How much time is left? What pace do I need to go from here to hit my goal? Can I go faster? Should I go slower?
+
+Doing a lot of this math is no problem early in a run. Not only is the math easier, but the mind is also still fresh. Also there's plenty of time to do long division in one's head. What else are you going to do all day?
+
+*2 miles in 22 minutes... so 11 minutes per mile. So for 50 miles that's 550 minutes. which is... um... 9 hours 10 minutes*
+
+That math is already hard, but a little ways into the run and the math gets a lot lot harder.
+`;
+
+const mathIsHardTxt2 = `
+*Okay. 7.5 miles in. I started at 7:13a, and it's now 8:35a. So that's an hour and 22 minutes. 72 minutes. 82 minutes per 7.5 miles is ... um... between 10 and 11 minutes/mile. so I'll be done in between 430 minutes and um... 500 minutes?  I'm either going too fast or too slow. I can't tell. Then what time will I be done? Oh, look I'm at 8 miles now. maybe this math will be easier.*
+
+Now imagine further and further into the run having to calculate at 17 miles, 29 miles, 43 miles. Not only is the math nearly impossible without a calculator (or at least pen and paper), but your mind is exhausted. 
+
+If only there was a machine that could do this math automatically...
+`;
+
+const aboutTxt = `
+The purpose of this app is to calculate stats for the runner and display it very easily.
+
+Before the run, the runner inputs
+
++ Target start time (e.g., sunrise)
++ Target completion time (e.g., sunset)
++ Goal distance in that time
+
+While running, the user can input the number of miles run so far. (Note: a separate solution is needed to calculate
+this distance, be that a GPS app or a GPS watch or mile markers). The app will then calculate the following
++ Distance
++ How far ahead (or behind) the goal pace
++ How far the runner has gone
++ How far is left until the goal distance
++ The project distance traveled at sunset, assuming average pace
++ Pace
++ The average pace since sunrise
++ How far ahead (or behind) the goal pace
++ How much time the runner can walk and still be on the goal pace
++ How much time the runner can rest and still be on the goal pace
++ Time
++ Duration elapsed since sunrise
++ Duration remaining until sunset
++ The estimated time at which the goal distance will be reached
++ The estimated duration until the goal distance will be reached
 
 
-let aboutTxt = <Text>
+Notes about using the app
++  Test the app yourself before your big run! Test it both on the couch, to see how it works, and test it
+on a shorter run to see how you like it. I don't recommend wrestling with it during an important run.
++ The calculated statistics only updates when the runner sets the distance; the app uses the time on the phone
+to calculate speeds.
++ When on the couch, use "Demo Mode" to set the time that the app will see.
++ The app wlil offer a ->| button to "fast forward" based upon expected pace. The intention there is to
+make it easy to set the completed distance.
++ The app is still useful if one runs after "sunset"; for my first run with this app, I was around 43 miles
+at sunset but had a goal of 52.5. The math it calculated was still helpful as I extended my run further.
 
-  The purpose of this app is to calculate all of that for the runner and display it very easily.
+This app is available free of charge. If you would like to donate, please send
+[$26.21 to the HWRTS podcast](https://howwasyourruntoday.com/donate').
 
-  Before the run, the runner inputs
-  * Target start time (e.g., sunrise)
-  * Target completion time (e.g., sunset)
-  * Goal distance in that time
-
-  While running, the user can input the number of miles run so far. (Note: a separate solution is needed to calculate
-  this distance, be that a GPS app or a GPS watch or mile markers). The app wil then calculate the following
-  * Distance
-  * How far ahead (or behind) the goal pace
-  * How far the runner has gone
-  * How far is left until the goal distance
-  * The project distance traveled at sunset, assuming average pace
-  * Pace
-  * The average pace since sunrise
-  * How far ahead (or behind) the goal pace
-  * How much time the runner can walk and still be on the goal pace
-  * How much time the runner can rest and still be on the goal pace
-  * Time
-  * Duration elapsed since sunrise
-  * Duration remaining until sunset
-  * The estimated time at which the goal distance will be reached
-  * The estimated duration until the goal distance will be reached
-
-
-  Notes about using the app
-  *  Test the app yourself before your big run! Test it both on the couch, to see how it works, and test it
-  on a shorter run to see how you like it. I don't recommend wrestling with it during an important run.
-  * The calculated statistics only updates when the runner sets the distance; the app uses the time on the phone
-  to calculate speeds.
-  * When on the couch, use "Demo Mode" to set the time that the app will see.
-  * The app wlil offer a ->| button to "fast forward" based upon expected pace. The intention there is to
-  make it easy to set the completed distance.
-  * The app is still useful if one runs after "sunset"; for my first run with this app, I was around 43 miles
-  at sunset but had a goal of 52.5. The math it calculated was still helpful as I extended my run further.
-
-  This app is available free of charge. If you would like to donate, please send
-  <Text style={{color: 'blue'}}
-        onPress={() => Linking.openURL('https://howwasyourruntoday.com/donate')}>
-    $26.21 to the HWYRT podcast
-  </Text>.
-
-  This app is also only available on Android. If you are an iOS developer or know an iOS developer, porting
-  to iOS will be *very easy*. The entire codebase is and only needs to be re-compiled for iOS and posted
-  on the App Store.
-  <Text style={{color: 'blue'}}
-        onPress={() => Linking.openURL('https://github.com/TinyTheBrontosaurus/ultra-clock')}>
-    available on GitHub
-  </Text>. Same offer is available for anyone who wants to clean up the graphics.
-</Text>;
+This app is also only available on Android. If you are an iOS developer or know an iOS developer, porting
+to iOS will be *very easy*. The entire codebase is and only needs to be re-compiled for iOS and posted
+on the App Store. [Code available on GitHub](https://github.com/TinyTheBrontosaurus/ultra-clock). Same offer is
+available for anyone who wants to clean up the graphics.
+`;
 
 
 const slides = [
   {
     key: 'one',
-    title: 'Welcome to the Soul-Search Ultra Marathon Clock!',
-    body:() =>  <Text>
-    The Soul-Searching Ultra Clock app is an extremely niche app meant for ultramarathoners who are running
-    a pre-specified amount of time. An example of this is a "Solstice Run" where a one runs from sunrise to
-    sunset. This app was inspired by the "How Was Your Run Today" podcast episode 216.5
-    <Text style={{color: 'blue'}}
-    onPress={() => Linking.openURL('https://howwasyourruntoday.com/episodes/hwyrt-holiday-bonus-bryans-soul-searching-s1!b121d')}>
-    HWYRT Holiday Bonus - Bryan's Soul Searching Solstice Run
-    </Text>
-    where host Bryan ran from sunrise to sunset. The author is this app ran a similar run on December 30, 2020
-    using this app and now wants to share it with the wider community.
-    </Text>,
+    title: 'Welcome to the\nSoul-Searching\nUltra Marathon Clock!',
+    body:() =>  <Markdown>{`
+The Soul-Searching Ultra Clock app is an **extremely** niche app.
+
+Not only is it for ultra runners, but specifically for ultra runners who are running for a pre-specified amount of time. The primary example of this is a "Solstice Run" where a one runs from sunrise to sunset. This app was inspired by the "How Was Your Run Today" podcast episode 216.5: [HWYRT Holiday Bonus - Bryan's Soul Searching Solstice Run](https://howwasyourruntoday.com/episodes/hwyrt-holiday-bonus-bryans-soul-searching-s1!b121d) where host Bryan ran from sunrise to sunset.
+
+The author is this app ran a similar run on December 30, 2020 using this app and is sharing it with the wider community.
+
+*Note this app is not affiliated with HWYRT other than the author listens to all of the episodes
+    `}</Markdown>,
     backgroundColor: '#59b2ab',
   },
   {
     key: 'two',
     title: 'Math is hard',
-    body: () => <Text>
-    Whenever I run, I am frequently doing math in my head. Calculating my pace. Calculating how far I have left to go.
-    How much time is left? What pace do I need to go from here to hit my goal? Can I go faster? slower?
-
-    Doing a lot of this math is no problem early in a run. No only is the math easier, but the mind is also still fresh.
-    <Text style="italic">2 miles in 22 minutes, 11 min/mile,
-    50 miles in 550 minutes which is... um... 9 hours 10 minutes</Text>
-    But a little ways into the run and the math gets a lot harder.
-    <Text style="italic">Okay. 7.5 miles in. I started at 7:13a, and it's now 8:35a. So that's an hour and 22 minutes. 72 minutes.
-    82 minutes per 7.5 miles is ... um... between 10 and 11 minutes/mile. so i'll be done in between 430 minutes and um... 500 minutes?
-    Then what time wlil I be done? Oh, look I'm at 8 miles now. maybe this math will be easier.
-    </Text>
-    Now imagine further and further into the run having to calculate at 17 miles, 29 miles, 43 miles. Not only is
-    the math nearly impossible, but your mind is exhausted.</Text>,
-    backgroundColor: '#febe29',
+    body: () => <Markdown>{mathIsHardTxt1}</Markdown>,
+    backgroundColor: '#22bcb5',
+  },
+  {
+    key: 'two-two',
+    title: 'Tired math is even harder',
+    body: () => <Markdown>{mathIsHardTxt2}</Markdown>,
+    backgroundColor: '#22bcb5',
   },
   {
     key: 'three',
-    title: 'Rocket guy',
-    body: () => aboutTxt,
+    title: 'How to use',
+    body: () => <Markdown>{aboutTxt}</Markdown>,
     backgroundColor: '#22bcb5',
   }
 ];
@@ -112,22 +115,13 @@ const slides = [
 const styles = StyleSheet.create({
   slide: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-  },
-  image: {
-    width: 320,
-    height: 320,
-    marginVertical: 32,
+    backgroundColor: "#ab87ab",
   },
   text: {
     color: 'rgba(255, 255, 255, 0.8)',
-    textAlign: 'center',
   },
   title: {
     fontSize: 22,
-    color: 'white',
     textAlign: 'center',
   },
 });
